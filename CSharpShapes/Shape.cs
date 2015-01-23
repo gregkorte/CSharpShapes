f77781eb28ce643c559202c79fa86baa1a949547
gregkorte <gryphionsroost@yahoo.com>
2015-01-23 11:24:21 -0600

    Added comments.

--- CSharpShapes/Shape.cs
+++ CSharpShapes/Shape.cs
@@ -10,17 +10,39 @@ namespace CSharpShapes
     public class Shape
     {
         // Width, Height, Perimeter, Area, BorderColor, FillColor, NumberOfSides
+
+        /// <summary>
+        /// The interior color of the shape, when drawn.
+        /// </summary>
         public Color FillColor { get; set; }
+
+        /// <summary>
+        /// The border color of the shape, when drawn.
+        /// </summary>
         public Color BorderColor { get; set; }
+
+        /// <summary>
+        /// The number of sides to the shape.
+        /// </summary>
         public virtual int SidesCount { get; }
 
-        // Returns the area of this shape
+        /// <summary>
+        /// Calculates the area of the shape.
+        /// </summary>
+        /// <returns>
+        /// The area of the shape.</returns>
         public abstract decimal Area();
 
-        // Returns the perimeter of this shape
+        /// <summary>
+        /// Calculates the perimeter of the shape.
+        /// </summary>
+        /// <returns>The perimeter of the shape</returns>
         public abstract int Perimeter();
-        
-        // Resizes a shape by percent%
-        public abstract void scale(int percent){ }
+
+        /// <summary>
+        /// Scales the shape in size.
+        /// </summary>
+        /// <param name="percentage">The percentage by which to scale the shape.</param>
+        public abstract void scale(int percent);
     }
 }
