367ce80feef4c2246230a315ea7b719763ddbe65
gregkorte <gryphionsroost@yahoo.com>
2015-01-23 10:12:32 -0600

    Added System.Drawing dependency reference.

---
+++ CSharpShapes/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CSharpShapes
+{
+    public class Shape
+    {
+        // Width, Height, Perimeter, Area, BorderColor, FillColor, NumberOfSides
+        public Color FillColor { get; set; }
+        public Color BorderColor { get; set; }
+        public virtual int SidesCount { get; }
+
+        // Returns the area of this shape
+        public abstract decimal Area();
+
+        // Returns the perimeter of this shape
+        public abstract int Perimeter();
+        
+        // Resizes a shape by percent%
+        public abstract void scale(int percent){ }
+    }
+}
