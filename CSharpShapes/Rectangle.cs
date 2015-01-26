d3540c945af7afd1218c97094e1d3291c61073ab
gregkorte <gryphionsroost@yahoo.com>
2015-01-26 16:17:14 -0600

    Implement Rectangle constructor.

---
+++ CSharpShapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpShapes
+{
+    public class Rectangle
+    {
+        private int width;
+        public int Width
+        {
+            get { return this.width; }
+        }
+
+        private int height;
+        public int Height
+        {
+            get { return this.height; }
+        }
+
+        public Rectangle(int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
