3f8f700b776169ecdbed73ab9593c0227d28c261
gregkorte <gryphionsroost@yahoo.com>
2015-01-29 12:05:01 -0600

    Draw shape button drawing all shapes.

---
+++ ShapeSketcher/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using CSharpShapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Drawing;
+
+namespace ShapeSketcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateClassList();
+        }
+
+        private void PopulateClassList()
+        {
+            var classList = new List<string>();
+            var shapeType = typeof(Shape);
+            foreach (Type type in Assembly.GetAssembly(shapeType).GetTypes())
+            {
+                if (type.IsSubclassOf(shapeType) && !type.IsAbstract)
+                {
+                    classList.Add(type.Name);
+                }
+            }
+            ComboBox.ItemsSource = classList;
+        }
+
+        private void DrawRectangle()
+        {
+            System.Windows.Shapes.Polygon myPolygon = new System.Windows.Shapes.Polygon();
+            myPolygon.Stroke = System.Windows.Media.Brushes.Tomato;
+            myPolygon.Fill = System.Windows.Media.Brushes.Bisque;
+            myPolygon.StrokeThickness = 2;
+            myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
+            myPolygon.VerticalAlignment = VerticalAlignment.Center;
+            System.Windows.Point Point1 = new System.Windows.Point(200, 250);
+            System.Windows.Point Point2 = new System.Windows.Point(200, 325);
+            System.Windows.Point Point3 = new System.Windows.Point(350, 400);
+            System.Windows.Point Point4 = new System.Windows.Point(350, 250);
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(Point1);
+            myPointCollection.Add(Point2);
+            myPointCollection.Add(Point3);
+            myPointCollection.Add(Point4);
+            myPolygon.Points = myPointCollection;
+            ShapeCanvas.Children.Add(myPolygon);
+        }
+
+        private void DrawSquare(int x, int y, Square square)
+        {
+            int size = (int)square.Width;
+            System.Windows.Shapes.Polygon myPolygon = new System.Windows.Shapes.Polygon();
+            myPolygon.Stroke = System.Windows.Media.Brushes.Chocolate;
+            myPolygon.Fill = System.Windows.Media.Brushes.BurlyWood;
+            myPolygon.StrokeThickness = 2;
+            myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
+            myPolygon.VerticalAlignment = VerticalAlignment.Center;
+            System.Windows.Point Point1 = new System.Windows.Point(x, y);
+            System.Windows.Point Point2 = new System.Windows.Point(x+size, y);
+            System.Windows.Point Point3 = new System.Windows.Point(x+size, y+size);
+            System.Windows.Point Point4 = new System.Windows.Point(x, y+size);
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(Point1);
+            myPointCollection.Add(Point2);
+            myPointCollection.Add(Point3);
+            myPointCollection.Add(Point4);
+            myPolygon.Points = myPointCollection;
+            ShapeCanvas.Children.Add(myPolygon);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DrawRectangle();
+            DrawSquare(1, 50, new Square(30));
+            Square square = new Square(200);
+            square.FillColor = System.Drawing.Color.NavajoWhite;
+            square.BorderColor = System.Drawing.Color.Moccasin;
+            DrawSquare(50, 5, square);
+        }
+    }
+}
+
